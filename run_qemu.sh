ccb00ec85d5fc2ee7e67a4ec9785b467dccfd79f
kkr <kkr@jubix.co.kr>
2022-05-09 15:00:52 +0900

    arm64 test중

--- run_qemu.sh
+++ run_qemu.sh
@@ -15,8 +15,9 @@ fi
 
 if [[ "$1" == *"arm64"* ]];then
 	echo arm64 mode
-	#https://m.blog.naver.com/rlawlxo1064/221621552737
-	qemu-system-aarch64 -smp 1 -rtc base=localtime -M virt -cpu cortex-a72 -m 512 -fda ${1}
+	# https://m.blog.naver.com/rlawlxo1064/221621552737
+	#qemu-system-aarch64 -smp 1 -rtc base=localtime -M virt -cpu cortex-a72 -m 512 -hda ${1}
+	qemu-system-aarch64 -smp 1 -machine virt -cpu cortex-a72 -m 512M -kernel "$1" -nographic -s
 	exit 0
 fi  
 
--- setup_env.sh
+++ setup_env.sh
@@ -1,2 +1,6 @@
 sudo apt install binutils bison flex libc6-dev libtool make patchutils libgmp-dev libmpfr-dev libmpc-dev nasm qemu-kvm
+
+## for arm64 
+# https://azeria-labs.com/arm-on-x86-qemu-user/
 #sudo apt-get install qemu-system-aarch64
+#sudo apt install qemu-user qemu-user-static gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu binutils-aarch64-linux-gnu-dbg build-essential
